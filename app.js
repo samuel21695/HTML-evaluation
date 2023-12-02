669c2e4b828e0bf3cee08e123b84d799535a6169
samuel21695 <samuel21695@gmail.com>
2023-12-02 23:36:59 +0900

    📝 create first dynamic section

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Create root element
   const rootElement = document.getElementById('root');
 
+  // Create the first dynamic section
+  const section1 = document.createElement('div');
+  section1.className = 'w-90vw h-40vh bgc-D9 display-f align-center justify-space-around';
 });
